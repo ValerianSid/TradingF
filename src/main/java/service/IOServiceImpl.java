4387ce32265b85510678518a66a5e96e10486ed1
Romashka <romash_om@mail.ru>
2022-08-15 00:25:24 +0300

    Добавлен ввод-вывод для консоли

---
+++ src/main/java/service/IOServiceImpl.java
@@ -0,0 +1,35 @@
+package service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class IOServiceImpl implements IOService {
+
+    private BufferedReader reader;
+
+    public IOServiceImpl() {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public String read() throws IOException {
+        return reader.readLine();
+    }
+
+    public int readInt() {
+        try {
+            return Integer.parseInt(read());
+        } catch (IOException e) {
+            writeUnknownError();
+            return readInt();
+        }
+    }
+
+    public void write(String message) {
+        System.out.println(message);
+    }
+
+    public void writeUnknownError() {
+        write("Неизвестная ошибка. Попробуйте еще раз");
+    }
+}
