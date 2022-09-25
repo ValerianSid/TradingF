3784cfef0128a6f742b950a787ef10fda6fc5b7f
anton <panant@mail.ru>
2022-09-25 20:04:19 +0300

    что то начудил)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 import exсeption.NoEnoughMoneyException;
+import javafx.application.Application;
 import manager.ForexApp;
 
 import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args) throws NoEnoughMoneyException, IOException {
-        ForexApp forexApp = new ForexApp();
-        forexApp.run();
-    }
-}
+import static javafx.application.Application.launch;
+
+//public class Main {
+    //public static void main(String[] args) throws NoEnoughMoneyException, IOException {
+        //ForexApp forexApp = new ForexApp();
+       // forexApp.run();
+        //Application.launch();
+   // }
+//}
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -9,4 +9,20 @@ public class User {
         this.name = name;
         this.wallet = wallet;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(Wallet wallet) {
+        this.wallet = wallet;
+    }
 }
